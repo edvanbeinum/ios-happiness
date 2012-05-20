7909a71f1459c496f3fd0fa5077b9dd2678683eb
Ed van Beinum <ed@vanbeinum.org>
2012-05-20 12:27:32 +0100

    add pan up and down gesture

--- StandfordLecture5/FaceView.m
+++ StandfordLecture5/FaceView.m
@@ -10,6 +10,8 @@
 
 @implementation FaceView
 
+@synthesize dataSource = _dataSource;
+
 @synthesize scale = _scale;
 
 #define DEFAULT_SCALE 0.90;
@@ -75,7 +77,7 @@
 {
     CGContextRef context = UIGraphicsGetCurrentContext();
     
-    //draw head
+    //draw head  
     CGPoint midPoint;
     midPoint.x = self.bounds.origin.x + self.bounds.size.width/2;
     midPoint.y = self.bounds.origin.y + self.bounds.size.height/2;
@@ -119,7 +121,9 @@
     CGPoint mouthCP2 = mouthEnd;
     mouthCP2.x -= MOUTH_H * size * 2/3;
     
-    float smile = 1.0;
+    float smile = [self.dataSource smileForFaceView:self];
+    if (smile < -1) smile = -1;
+    if (smile > 1) smile = 1;
     
     CGFloat smileOffset = MOUTH_SMILE * size * smile;
     mouthCP1.y += smileOffset;
@@ -127,7 +131,7 @@
     
     CGContextBeginPath(context);
     CGContextMoveToPoint(context,mouthStart.x, mouthStart.y);
-    CGContextAddCurveToPoint(context, mouthCP1.x, mouthCP1.y, mouthCP2.x, mouthCP2.y, mouthEnd.x, mouthEnd.y);
+    CGContextAddCurveToPoint(context, mouthCP1.x, mouthCP1.y, mouthCP2.x, mouthCP2.y, mouthEnd.x, mouthEnd.y); //bezier curve
     CGContextStrokePath(context);
 }
 
